d80688a14fad72dd9c4a62e2fc9ca26cac0a21bd
Donstesh <stephenshifoko@gmail.com>
2023-09-23 07:45:11 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', 'ProductController@index')->name('products.index');
+Route::get('/products/create', 'ProductController@create')->name('products.create');
+Route::post('/products', 'ProductController@store')->name('products.store');
+Route::get('/products/{product}/edit', 'ProductController@edit')->name('products.edit');
+Route::put('/products/{product}', 'ProductController@update')->name('products.update');
+Route::delete('/products/{product}', 'ProductController@destroy')->name('products.destroy');
+
+Route::get('/orders', 'OrderController@index')->name('orders.index');
+Route::get('/orders/create', 'OrderController@create')->name('orders.create');
+Route::post('/orders', 'OrderController@store')->name('orders.store');
+Route::get('/orders/{order}', 'OrderController@show')->name('orders.show');
+
